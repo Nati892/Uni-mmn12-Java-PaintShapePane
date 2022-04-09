0c3790aa3327f8f49dc021aad8ce251c28d447ad
Nati892 <netanel892@gmail.com>
2022-04-09 20:13:38 +0300

    save

--- src/MyLayoutController.java
+++ src/MyLayoutController.java
@@ -10,6 +10,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 
 public class MyLayoutController {
@@ -38,43 +41,49 @@ public class MyLayoutController {
     private ToggleButton shape_fill_select;
 
     @FXML
-    private ComboBox<?> shape_selector;
+    private ComboBox<String> shape_selector;
 
     @FXML
     private Button undo_button;
 
 
+    @FXML
+    public void initialize() {
+        shape_selector.getItems().addAll(
+                "jacob.smith@example.com",
+                "isabella.johnson@example.com",
+                "ethan.williams@example.com",
+                "emma.jones@example.com",
+                "michael.brown@example.com");
+
+    }
+
+    @FXML
+    void clearOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    void paintPaneOnDragDetected(MouseEvent event) {
+    }
 
     @FXML
     void paintPaneOnMouseClicked(MouseEvent event) {
-
     }
 
     @FXML
     void paintPaneOnMouseDragged(MouseEvent event) {
-
     }
 
     @FXML
     void paintPaneOnMousePressed(MouseEvent event) {
-
     }
 
     @FXML
     void paintPaneOnMouseReleased(MouseEvent event) {
-
-    }
-
-
-    @FXML
-    void clearOnClick(ActionEvent event) {
-
-
     }
 
     @FXML
     void undoOnClick(ActionEvent event) {
-
 
     }
 
